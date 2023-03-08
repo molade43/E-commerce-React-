b2f55881091397b16debb219cf15b29712a1feb3
molade43 <molade43@gmail.com>
2023-03-08 15:59:09 +0100

    further worked on creating {categorries which was passed to cateoryItem using props} {products which was passed down to product using props} lastly newsletter).

---
+++ src/components/NewsLetter.jsx
@@ -0,0 +1,61 @@
+import { Send } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60vh;
+  background-color: #fcf5f5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const Title = styled.h1`
+  font-size: 70px;
+  margin-bottom: 20px;
+`;
+const Description = styled.div`
+  font-size: 24px;
+  font-weight: 300;
+  margin-bottom: 20px;
+`;
+const InputContainer = styled.div`
+  width: 50%;
+  height: 40px;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid lightgray;
+`;
+const Input = styled.input`
+  border: none;
+  flex: 8;
+  padding-left: 10px;
+`;
+const Button = styled.div`
+  flex: 1;
+  border: none;
+  background-color: teal;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const NewsLetter = () => {
+  return (
+    <Container>
+      <Title> NewsLetter</Title>
+      <Description>
+        Get timely updates from your favourite products.
+      </Description>
+      <InputContainer>
+        <Input placeholder="Get Your Promotional and Update Email from Us" />
+        <Button>
+          <Send />
+        </Button>
+      </InputContainer>
+    </Container>
+  );
+};
+
+export default NewsLetter;
--- src/data.js
+++ src/data.js
@@ -24,3 +24,63 @@ export const sliderItems = [
     bg: "fbf0f4",
   },
 ];
+
+export const categories = [
+  {
+    id: 1,
+    img:
+      "https://cdn.shopify.com/s/files/1/0692/4255/products/Black-Suit---1_9f608683-2129-4321-8cd2-47efa69981b3_1000x.jpg?v=1574775511",
+    title: "SUMMER SALES",
+    description: "DON'T COMPROMISE ON STYLE! GET YOURS AT 30% OFF OLD STOCKS",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img:
+      "https://cdn.shopify.com/s/files/1/0692/4255/products/BlueChalkStripe_1_dac9cbfb-2ed0-4087-a2d4-23b782ce3767_600x.jpg?v=1675786293",
+    title: "DRY SEASON SALES",
+    description: "DON'T COMPROMISE ON STYLE! GET YOURS AT 30% OFF OLD STOCKS",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img:
+      "https://cdn.shopify.com/s/files/1/0692/4255/products/HeatheredNavySuit11_400x.jpg?v=1663620363",
+    title: "HARMATTAN SEASON SALES",
+    description: "DON'T COMPROMISE ON STYLE! GET YOURS AT 30% OFF OLD STOCKS",
+    bg: "fbf0f4",
+  },
+];
+
+export const popularProduct = [
+  {
+    id: 1,
+    img:
+      "https://cdn.shopify.com/s/files/1/0692/4255/products/Black-Suit---1_9f608683-2129-4321-8cd2-47efa69981b3_1000x.jpg?v=1574775511",
+  },
+  {
+    id: 2,
+    img:
+      "https://cdn.shopify.com/s/files/1/0692/4255/products/BlueChalkStripe_1_dac9cbfb-2ed0-4087-a2d4-23b782ce3767_600x.jpg?v=1675786293",
+  },
+  {
+    id: 3,
+    img:
+      "https://cdn.shopify.com/s/files/1/0692/4255/products/HeatheredNavySuit11_400x.jpg?v=1663620363",
+  },
+  {
+    id: 4,
+    img:
+      "https://cdn.shopify.com/s/files/1/0692/4255/products/Black-Suit---1_9f608683-2129-4321-8cd2-47efa69981b3_1000x.jpg?v=1574775511",
+  },
+  {
+    id: 5,
+    img:
+      "https://cdn.shopify.com/s/files/1/0692/4255/products/BlueChalkStripe_1_dac9cbfb-2ed0-4087-a2d4-23b782ce3767_600x.jpg?v=1675786293",
+  },
+  {
+    id: 6,
+    img:
+      "https://cdn.shopify.com/s/files/1/0692/4255/products/HeatheredNavySuit11_400x.jpg?v=1663620363",
+  },
+];
